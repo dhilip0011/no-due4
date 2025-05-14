b4a4e828a17d9f485804c38cbbd571eafafa383f
dhilip0011 <ddhilipprasath@gmail.com>
2025-05-14 19:34:46 +0530

    Update settings.py

--- NoDue/settings.py
+++ NoDue/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xyb$3(05$aucm1o(3jix@2j$y@l!veafs7jhzf$@$j_z41@dhb'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com','localhost','127.0.0.1]
+ALLOWED_HOSTS = ['.onrender.com','localhost']
 
 
 # Application definition
